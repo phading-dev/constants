a5e2e59e64aed54fda26a13f2b2c91056653bdaa
Yuxuan Xie <teststaybaka@gmail.com>
2024-09-12 14:10:19 -0700

    Add base url for all services. Release

--- origin.ts
+++ origin.ts
@@ -1,2 +1,10 @@
 export let ORIGIN_PROD = "www.phading.net";
 export let ORIGIN_DEV = "dev.phading.net";
+export let COMMENT_SERVICE_BASE_URL = "/ct";
+export let COMMERCE_SERVICE_BASE_URL = "/cr";
+export let PRODUCT_METER_SERVICE_BASE_URL = "/pm";
+export let PRODUCT_RECOMMENDATION_SERVICE_BASE_URL = "/pr";
+export let PRODUCT_SERVICE_BASE_URL = "/p";
+export let USER_ACTIVITY_SERVICE_BASE_URL = "/ua";
+export let USER_SERVICE_BASE_URL = "/u";
+export let USER_SESSION_SERVICE_BASE_URL = "/us";
