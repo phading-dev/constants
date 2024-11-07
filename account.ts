c6df17fa06220c8c0912c77888c3273b8f9fd914
Yuxuan Xie <teststaybaka@gmail.com>
2024-11-07 15:13:35 -0800

    Fixed typo. Release

--- account.ts
+++ account.ts
@@ -1,7 +1,7 @@
 export let USERNAME_LIMIT = 100;
 export let PASSWORD_LIMIT = 100;
 export let EMAIL_LIMIT = 200;
-export let NATURAL_NAME = 100;
+export let NATURAL_NAME_LIMIT = 100;
 export let DESCRIPTION_LIMIT = 2000;
 export let AVATAR_SIZE_LIMIT = 50 * 1024 * 1024; // 50 MB
 export let LARGE_AVATAR_SIZE = 100; // px
