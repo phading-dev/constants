2b1f1673f0392b950f92959c304337e4cd23b1a9
Yuxuan Xie <teststaybaka@gmail.com>
2024-11-07 15:06:22 -0800

    Added consts for accounts. Release

---
+++ account.ts
@@ -0,0 +1,7 @@
+export let USERNAME_LIMIT = 100;
+export let EMAIL_LIMIT = 200;
+export let NATURAL_NAME = 100;
+export let DESCRIPTION_LIMIT = 2000;
+export let AVATAR_SIZE_LIMIT = 50 * 1024 * 1024; // 50 MB
+export let LARGE_AVATAR_SIZE = 100; // px
+export let SMALL_AVATAR_SIZE = 40; // px
