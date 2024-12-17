f68080c12f969d2b7c60d6580ecf4a4f45416524
Yuxuan Xie <ykuyo.dev@gmail.com>
2024-12-16 18:02:17 -0800

    Add types and length limit to videos. Release

--- video.ts
+++ video.ts
@@ -1,2 +1,6 @@
 export let AUDIO_TRACKS_LIMIT = 32;
 export let SUBTITLE_TRACKS_LIMIT = 32;
+export let MEDIA_CONTENT_LENGTH_LIMIT = 50 * 1024 * 1024 * 1024; // 50GB
+export let ACCEPTED_MEDIA_TYPES = new Set(["mp4", "mov", "mkv", "aac"]);
+export let SUBTITLE_ZIP_CONTENT_LENGTH_LIMIT = 50 * 1024 * 1024; // 10MB
+export let ACCEPTED_SUBTITLE_ZIP_TYPES = new Set(["zip"]);
