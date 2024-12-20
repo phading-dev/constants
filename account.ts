39e4c9f778d575240096541f43d15069eb235276
Yuxuan Xie <ykuyo.dev@gmail.com>
2024-12-19 22:24:52 -0800

    Rename _LIMIT to MAX_. Added consts for show. Release

--- account.ts
+++ account.ts
@@ -1,8 +1,8 @@
-export let USERNAME_LIMIT = 100;
-export let PASSWORD_LIMIT = 100;
-export let EMAIL_LIMIT = 200;
-export let NATURAL_NAME_LIMIT = 100;
-export let DESCRIPTION_LIMIT = 2000;
-export let AVATAR_SIZE_LIMIT = 50 * 1024 * 1024; // 50 MB
+export let MAX_USERNAME_LENGTH = 100;
+export let MAX_PASSWORD_LENGTH = 100;
+export let MAX_EMAIL_LENGTH = 200;
+export let MAX_NATURAL_NAME_LENGTH = 100;
+export let MAX_DESCRIPTION_LENGTH = 2000;
+export let MAX_AVATAR_SIZE = 50 * 1024 * 1024; // 50 MB
 export let LARGE_AVATAR_SIZE = 100; // px
 export let SMALL_AVATAR_SIZE = 40; // px
--- video.ts
+++ video.ts
@@ -1,6 +1,6 @@
-export let AUDIO_TRACKS_LIMIT = 32;
-export let SUBTITLE_TRACKS_LIMIT = 32;
-export let MEDIA_CONTENT_LENGTH_LIMIT = 50 * 1024 * 1024 * 1024; // 50GB
+export let MAX_NUM_OF_AUDIO_TRACKS = 32;
+export let MAX_NUM_OF_SUBTITLE_TRACKS = 32;
+export let MAX_MEDIA_CONTENT_LENGTH = 50 * 1024 * 1024 * 1024; // 50GB
 export let ACCEPTED_MEDIA_TYPES = new Set(["mp4", "mov", "mkv", "aac"]);
-export let SUBTITLE_ZIP_CONTENT_LENGTH_LIMIT = 50 * 1024 * 1024; // 10MB
+export let MAX_SUBTITLE_ZIP_CONTENT_LENGTH = 50 * 1024 * 1024; // 10MB
 export let ACCEPTED_SUBTITLE_ZIP_TYPES = new Set(["zip"]);
