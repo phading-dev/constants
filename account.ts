2b0040e357a2aa24464c19d18457cd6a64db31c9
Ykuyo <ykuyo.dev@gmail.com>
2025-07-20 18:04:32 -0700

    Renamed natural name with name. Release

--- account.ts
+++ account.ts
@@ -1,6 +1,6 @@
 export let MAX_PASSWORD_LENGTH = 100;
 export let MAX_EMAIL_LENGTH = 200;
-export let MAX_NATURAL_NAME_LENGTH = 70;
+export let MAX_NAME_LENGTH = 100;
 export let MAX_DESCRIPTION_LENGTH = 2000;
 export let MAX_AVATAR_SIZE = 50 * 1024 * 1024; // 50 MB
 export let LARGE_AVATAR_SIZE = 100; // px
