dc2982904c62565c39f33d80cd78046693db360e
Ykuyo <ykuyo.dev@gmail.com>
2025-07-26 21:57:21 -0700

    Removed mkv from accepted video types. Release

--- video.ts
+++ video.ts
@@ -1,7 +1,7 @@
 export let MAX_NUM_OF_AUDIO_TRACKS = 32;
 export let MAX_NUM_OF_SUBTITLE_TRACKS = 32;
 export let MAX_MEDIA_CONTENT_LENGTH = 50 * 1024 * 1024 * 1024; // 50GB
-export let ACCEPTED_VIDEO_TYPES = new Set(["mp4", "mov", "mkv"]);
+export let ACCEPTED_VIDEO_TYPES = new Set(["mp4", "mov"]);
 export let ACCEPTED_AUDIO_TYPES = new Set(["m4a", "aac"]);
 export let MAX_SUBTITLE_ZIP_CONTENT_LENGTH = 50 * 1024 * 1024; // 10MB
 export let ACCEPTED_SUBTITLE_ZIP_TYPES = new Set(["zip"]);
